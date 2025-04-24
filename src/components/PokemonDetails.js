eca852dec123bbb469e6064a68e2a2c64db07f04
Dongsusin <auroratime020715@gmail.com>
2025-04-24 13:17:06 +0900

    상세정보 이동 버튼 추가

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./PokemonDetails.css";
+import { Link } from "react-router-dom";
 
 const PokemonDetails = ({ pokemon }) => {
   //포켓몬 데이터가 없으면 로딩 중
@@ -49,45 +50,169 @@ const PokemonDetails = ({ pokemon }) => {
       value: pokemon.stats[5].base_stat,
     },
   ];
-  return (
+  const isfirst = pokemon.id === 1;
+  const islast = pokemon.id === 1025;
+  return isfirst ? (
     <div className="pokemon-details">
-      <h2>
-        {pokemon.korean_name} (#{pokemon.id})
-      </h2>
-      <div className="details-image">
-        <div className="defualt">
-          <img src={pokemon.sprites.front_default} alt={pokemon.korean_name} />
-          <img src={pokemon.sprites.back_default} alt={pokemon.korean_name} />
-        </div>
-        <div className="shiny">
-          <img src={pokemon.sprites.front_shiny} alt={pokemon.korean_name} />
-          <img src={pokemon.sprites.back_shiny} alt={pokemon.korean_name} />
-        </div>
+      <div className="details-title">
+        <h2>
+          {pokemon.korean_name} (#{pokemon.id})
+        </h2>
+        <Link to={`/`} className="home"></Link>
       </div>
-      <div className="details-about">
-        <p>이름: {pokemon.korean_name}</p>
-        <p>키: {pokemon.height / 10 + "m"}</p>
-        <p>무게: {pokemon.weight / 10 + "kg"}</p>
-        <p>속성: {renderTypes()}</p>
-        <p>특성: {renderAbilities()}</p>
-      </div>
-      <div className="details-stats">
-        <h3>스탯</h3>
-        <div className="stats-list-data">
-          {statList.map((stat, index) => (
-            <div key={index} className="stats-list">
-              <p>{stat.name}</p>
-              <p>{stat.value}</p>
-              <div className="stats-bar">
-                <div
-                  className="stats-bar-fill"
-                  style={{ width: `${stat.value / 2}%` }}
-                ></div>
+      <div className="details">
+        <div className="details-image">
+          <div className="defualt">
+            <img
+              src={pokemon.sprites.front_default}
+              alt={pokemon.korean_name}
+            />
+            <img src={pokemon.sprites.back_default} alt={pokemon.korean_name} />
+          </div>
+          <div className="shiny">
+            <img src={pokemon.sprites.front_shiny} alt={pokemon.korean_name} />
+            <img src={pokemon.sprites.back_shiny} alt={pokemon.korean_name} />
+          </div>
+        </div>
+        <div className="details-about">
+          <p>이름: {pokemon.korean_name}</p>
+          <p>키: {pokemon.height / 10 + "m"}</p>
+          <p>무게: {pokemon.weight / 10 + "kg"}</p>
+          <p>속성: {renderTypes()}</p>
+          <p>특성: {renderAbilities()}</p>
+        </div>
+        <div className="details-stats">
+          <h3>스탯</h3>
+          <div className="stats-list-data">
+            {statList.map((stat, index) => (
+              <div key={index} className="stats-list">
+                <p>{stat.name}</p>
+                <p>{stat.value}</p>
+                <div className="stats-bar">
+                  <div
+                    className="stats-bar-fill"
+                    style={{ width: `${stat.value / 2}%` }}
+                  ></div>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
+      <p className="button">
+        <Link to={`/pokemon/${pokemon.id + 1}`}>
+          <span className="next" />
+        </Link>
+      </p>
+    </div>
+  ) : islast ? (
+    <div className="pokemon-details">
+      <div className="details-title">
+        <h2>
+          {pokemon.korean_name} (#{pokemon.id})
+        </h2>
+        <Link to={`/`} className="home"></Link>
+      </div>
+      <div className="details">
+        <div className="details-image">
+          <div className="defualt">
+            <img
+              src={pokemon.sprites.front_default}
+              alt={pokemon.korean_name}
+            />
+            <img src={pokemon.sprites.back_default} alt={pokemon.korean_name} />
+          </div>
+          <div className="shiny">
+            <img src={pokemon.sprites.front_shiny} alt={pokemon.korean_name} />
+            <img src={pokemon.sprites.back_shiny} alt={pokemon.korean_name} />
+          </div>
+        </div>
+        <div className="details-about">
+          <p>이름: {pokemon.korean_name}</p>
+          <p>키: {pokemon.height / 10 + "m"}</p>
+          <p>무게: {pokemon.weight / 10 + "kg"}</p>
+          <p>속성: {renderTypes()}</p>
+          <p>특성: {renderAbilities()}</p>
+        </div>
+        <div className="details-stats">
+          <h3>스탯</h3>
+          <div className="stats-list-data">
+            {statList.map((stat, index) => (
+              <div key={index} className="stats-list">
+                <p>{stat.name}</p>
+                <p>{stat.value}</p>
+                <div className="stats-bar">
+                  <div
+                    className="stats-bar-fill"
+                    style={{ width: `${stat.value / 2}%` }}
+                  ></div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <p className="button">
+        <Link to={`/pokemon/${pokemon.id - 1}`}>
+          <span className="prev" />
+        </Link>
+      </p>
+    </div>
+  ) : (
+    <div className="pokemon-details">
+      <div className="details-title">
+        <h2>
+          {pokemon.korean_name} (#{pokemon.id})
+        </h2>
+        <Link to={`/`} className="home"></Link>
+      </div>
+      <div className="details">
+        <div className="details-image">
+          <div className="defualt">
+            <img
+              src={pokemon.sprites.front_default}
+              alt={pokemon.korean_name}
+            />
+            <img src={pokemon.sprites.back_default} alt={pokemon.korean_name} />
+          </div>
+          <div className="shiny">
+            <img src={pokemon.sprites.front_shiny} alt={pokemon.korean_name} />
+            <img src={pokemon.sprites.back_shiny} alt={pokemon.korean_name} />
+          </div>
+        </div>
+        <div className="details-about">
+          <p>이름: {pokemon.korean_name}</p>
+          <p>키: {pokemon.height / 10 + "m"}</p>
+          <p>무게: {pokemon.weight / 10 + "kg"}</p>
+          <p>속성: {renderTypes()}</p>
+          <p>특성: {renderAbilities()}</p>
+        </div>
+        <div className="details-stats">
+          <h3>스탯</h3>
+          <div className="stats-list-data">
+            {statList.map((stat, index) => (
+              <div key={index} className="stats-list">
+                <p>{stat.name}</p>
+                <p>{stat.value}</p>
+                <div className="stats-bar">
+                  <div
+                    className="stats-bar-fill"
+                    style={{ width: `${stat.value / 2}%` }}
+                  ></div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <p className="button">
+        <Link to={`/pokemon/${pokemon.id - 1}`}>
+          <span className="prev" />
+        </Link>
+        <Link to={`/pokemon/${pokemon.id + 1}`}>
+          <span className="next" />
+        </Link>
+      </p>
     </div>
   );
 };
