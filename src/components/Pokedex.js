17dad366c390c57fd1ec52d4a7cda7d16b7d8f38
Dongsusin <auroratime020715@gmail.com>
2025-04-28 12:35:59 +0900

    타입별검색,한글검색,애니메이션 추가

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./pokedex.css";
 import { Link } from "react-router-dom";
 import InfiniteScroll from "react-infinite-scroll-component";
+import "./pokedex.css";
 
 const Pokedex = () => {
   const [pokemonData, setPokemonData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [filteredPokemonData, setFilteredPokemonData] = useState([]);
   const [searchError, setSearchError] = useState(false);
+  const [selectedType, setSelectedType] = useState("all"); // 속성 필터링
   const pokemonPerPage = 30;
   const totalPokemon = 1025;
 
@@ -45,25 +46,48 @@ const Pokedex = () => {
   const handleSearch = (e) => {
     const searchTerm = e.target.value.toLowerCase();
     const hasKorean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(searchTerm);
+
     if (hasKorean) {
-      alert("포켓몬 이름을 영어로 검색하세요.");
-      return;
-    }
-    const filteredPokemon = pokemonData.filter((pokemon) =>
-      pokemon.name.toLowerCase().includes(searchTerm)
-    );
+      const filteredPokemon = pokemonData.filter((pokemon) =>
+        pokemon.korean_name.toLowerCase().includes(searchTerm)
+      );
 
-    if (filteredPokemon.length === 0) {
-      setSearchError(true);
+      if (filteredPokemon.length === 0) {
+        setSearchError(true);
+      } else {
+        setSearchError(false);
+      }
+
+      setFilteredPokemonData(filteredPokemon);
     } else {
-      setSearchError(false);
-    }
+      const filteredPokemon = pokemonData.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(searchTerm)
+      );
 
-    setFilteredPokemonData(filteredPokemon);
+      if (filteredPokemon.length === 0) {
+        setSearchError(true);
+      } else {
+        setSearchError(false);
+      }
+
+      setFilteredPokemonData(filteredPokemon);
+    }
+  };
+
+  const handleTypeFilter = (type) => {
+    setSelectedType(type);
+    if (type === "all") {
+      setFilteredPokemonData(pokemonData);
+    } else {
+      const filteredByType = pokemonData.filter((pokemon) =>
+        pokemon.types.some((t) => t.type.name === type)
+      );
+      setFilteredPokemonData(filteredByType);
+    }
   };
 
   if (pokemonData.length === 0) {
-    return <h1 className="all-loaded">Loading...</h1>;
+    return <div className="loading-spinner">로딩 중...</div>;
   }
 
   return (
@@ -72,23 +96,65 @@ const Pokedex = () => {
         dataLength={filteredPokemonData.length}
         next={fetchMoreData}
         hasMore={currentPage * pokemonPerPage < totalPokemon}
-        loader={<h4 className="loaded">Loading...</h4>}
+        loader={<h4 className="loaded">로딩 중...</h4>}
       >
         <div className="header">
           <h1 className="title">포켓몬 도감</h1>
           <input
             type="text"
             className="search"
-            placeholder="포켓몬 이름을 검색하세요(영문).(한글개발중)"
+            placeholder="포켓몬 이름을 검색하세요(영문/한글)"
             onChange={handleSearch}
           />
+          <div className="filter">
+            <button
+              className={`filter-btn ${selectedType === "all" ? "active" : ""}`}
+              onClick={() => handleTypeFilter("all")}
+            >
+              전체
+            </button>
+            {[
+              "fire",
+              "water",
+              "grass",
+              "electric",
+              "bug",
+              "normal",
+              "poison",
+              "fighting",
+              "psychic",
+              "ghost",
+              "dark",
+              "fairy",
+              "dragon",
+              "rock",
+              "ice",
+              "ground",
+              "steel",
+              "flying",
+            ].map((type) => (
+              <button
+                key={type}
+                className={`filter-btn ${
+                  selectedType === type ? "active" : ""
+                }`}
+                onClick={() => handleTypeFilter(type)}
+              >
+                {getKoreanTypeName(type)}
+              </button>
+            ))}
+          </div>
         </div>
         <div className="container">
           {searchError ? (
             <h2 className="centered-message">해당하는 포켓몬이 없습니다.</h2>
           ) : (
             filteredPokemonData.map((pokemon) => (
-              <div key={pokemon.id} className="pokemon">
+              <div
+                key={pokemon.id}
+                className="pokemon"
+                style={{ animation: "fadeIn 0.5s ease-in" }}
+              >
                 <Link to={`/pokemon/${pokemon.id}`}>
                   <img
                     className="image"
@@ -101,6 +167,19 @@ const Pokedex = () => {
                       <p>({pokemon.name})</p>
                     </div>
                     <p>도감번호:{pokemon.id}</p>
+                    <div className="types">
+                      {pokemon.types.map((typeInfo) => (
+                        <span
+                          key={typeInfo.type.name}
+                          className="type"
+                          style={{
+                            backgroundColor: getTypeColor(typeInfo.type.name),
+                          }}
+                        >
+                          {getKoreanTypeName(typeInfo.type.name)}
+                        </span>
+                      ))}
+                    </div>
                   </div>
                 </Link>
               </div>
@@ -110,6 +189,92 @@ const Pokedex = () => {
       </InfiniteScroll>
     </>
   );
+};
+
+const getTypeColor = (type) => {
+  switch (type) {
+    case "fire":
+      return "#F08030"; // 불꽃
+    case "water":
+      return "#6890F0"; // 물
+    case "grass":
+      return "#78C850"; // 풀
+    case "electric":
+      return "#F8D030"; // 전기
+    case "bug":
+      return "#A8B820"; // 벌레
+    case "normal":
+      return "#A8A878"; // 노말
+    case "poison":
+      return "#A040A0"; // 독
+    case "fighting":
+      return "#C03028"; // 격투
+    case "psychic":
+      return "#F85888"; // 에스퍼
+    case "ghost":
+      return "#705898"; // 고스트
+    case "dark":
+      return "#705848"; // 어둠
+    case "fairy":
+      return "#EE99AC"; // 요정
+    case "dragon":
+      return "#7038F8"; // 드래곤
+    case "rock":
+      return "#B8A038"; // 바위
+    case "ice":
+      return "#98D8D8"; // 얼음
+    case "ground":
+      return "#E0C068"; // 땅
+    case "steel":
+      return "#B8B8D0"; // 강철
+    case "flying":
+      return "#A890F0"; // 비행
+    default:
+      return "#FFFFFF"; // 기본 흰색
+  }
+};
+
+const getKoreanTypeName = (type) => {
+  switch (type) {
+    case "fire":
+      return "불꽃";
+    case "water":
+      return "물";
+    case "grass":
+      return "풀";
+    case "electric":
+      return "전기";
+    case "bug":
+      return "벌레";
+    case "normal":
+      return "노말";
+    case "poison":
+      return "독";
+    case "fighting":
+      return "격투";
+    case "psychic":
+      return "에스퍼";
+    case "ghost":
+      return "고스트";
+    case "dark":
+      return "어둠";
+    case "fairy":
+      return "요정";
+    case "dragon":
+      return "드래곤";
+    case "rock":
+      return "바위";
+    case "ice":
+      return "얼음";
+    case "ground":
+      return "땅";
+    case "steel":
+      return "강철";
+    case "flying":
+      return "비행";
+    default:
+      return type; // 기본값은 영어로 반환
+  }
 };
 
 export default Pokedex;
--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -1,11 +1,20 @@
-import React from "react";
-import "./PokemonDetails.css";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "./PokemonDetails.css";
 
 const PokemonDetails = ({ pokemon }) => {
+  const [transition, setTransition] = useState(false);
+
+  useEffect(() => {
+    setTransition(true);
+    const timer = setTimeout(() => setTransition(false), 300);
+    return () => clearTimeout(timer);
+  }, [pokemon]);
+
   if (!pokemon) {
     return <p>Loading...</p>;
   }
+
   const renderTypes = () => {
     return pokemon.types.map((type, index) => (
       <span
@@ -17,27 +26,31 @@ const PokemonDetails = ({ pokemon }) => {
       </span>
     ));
   };
+
   const getTypeBackgroundColor = (typeName) => {
     const typeColors = {
-      fire: "#f54242",
-      water: "#4287f5",
-      grass: "#42f554",
-      electric: "#f5e142",
-      bug: "#a8e200",
-      normal: "#a8a8a8",
-      psychic: "#f542bb",
-      fighting: "#f54242",
-      poison: "#8e42f5",
-      ground: "#e5c17b",
-      rock: "#8e7f72",
-      ghost: "#7a42f5",
-      dragon: "#6a42f5",
-      ice: "#42e5f5",
-      fairy: "#f542d8",
-      dark: "#585858",
+      fire: "#F08030",
+      water: "#6890F0",
+      grass: "#78C850",
+      electric: "#F8D030",
+      bug: "#A8B820",
+      normal: "#A8A878",
+      psychic: "#F85888",
+      fighting: "#C03028",
+      poison: "#A040A0",
+      ground: "#E0C068",
+      rock: "#B8A038",
+      ghost: "#705898",
+      dragon: "#7038F8",
+      ice: "#98D8D8",
+      fairy: "#EE99AC",
+      dark: "#705848",
+      steel: "#B8B8D0",
+      flying: "#A890F0",
     };
     return typeColors[typeName] || "#e8e8e8";
   };
+
   const renderAbilities = () => {
     return pokemon.abilities.map((ability, index) => (
       <span key={ability.ability.name} className="ability">
@@ -45,6 +58,7 @@ const PokemonDetails = ({ pokemon }) => {
       </span>
     ));
   };
+
   const statList = [
     {
       name: "hp",
@@ -75,8 +89,8 @@ const PokemonDetails = ({ pokemon }) => {
   const isfirst = pokemon.id === 1;
   const islast = pokemon.id === 1025;
 
-  return isfirst ? (
-    <div className="pokemon-details">
+  return (
+    <div className={`pokemon-details ${transition ? "fade-in" : ""}`}>
       <div className="details-title">
         <h2>
           {pokemon.korean_name} (#{pokemon.id})
@@ -125,122 +139,24 @@ const PokemonDetails = ({ pokemon }) => {
         </div>
       </div>
       <p className="button">
-        <Link to={`/pokemon/${pokemon.id + 1}`}>
-          <span className="next" />
-        </Link>
-      </p>
-    </div>
-  ) : islast ? (
-    <div className="pokemon-details">
-      <div className="details-title">
-        <h2>
-          {pokemon.korean_name} (#{pokemon.id})
-        </h2>
-        <Link to={`/`} className="home"></Link>
-      </div>
-      <div className="details">
-        <div className="details-image">
-          <div className="defualt">
-            <img
-              src={pokemon.sprites.front_default}
-              alt={pokemon.korean_name}
-            />
-            <img src={pokemon.sprites.back_default} alt={pokemon.korean_name} />
-          </div>
-          <div className="shiny">
-            <img src={pokemon.sprites.front_shiny} alt={pokemon.korean_name} />
-            <img src={pokemon.sprites.back_shiny} alt={pokemon.korean_name} />
-          </div>
-        </div>
-        <div className="details-about">
-          <p>
-            이름: {pokemon.korean_name}({pokemon.name})
-          </p>
-          <p>키: {pokemon.height / 10 + "m"}</p>
-          <p>무게: {pokemon.weight / 10 + "kg"}</p>
-          <p>속성: {renderTypes()}</p>
-          <p>특성: {renderAbilities()}</p>
-        </div>
-        <div className="details-stats">
-          <h3>스탯</h3>
-          <div className="stats-list-data">
-            {statList.map((stat, index) => (
-              <div key={index} className="stats-list">
-                <p>{stat.name}</p>
-                <p>{stat.value}</p>
-                <div className="stats-bar">
-                  <div
-                    className="stats-bar-fill"
-                    style={{ width: `${stat.value / 2}%` }}
-                  ></div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-      <p className="button">
-        <Link to={`/pokemon/${pokemon.id - 1}`}>
-          <span className="prev" />
-        </Link>
-      </p>
-    </div>
-  ) : (
-    <div className="pokemon-details">
-      <div className="details-title">
-        <h2>
-          {pokemon.korean_name} (#{pokemon.id})
-        </h2>
-        <Link to={`/`} className="home"></Link>
-      </div>
-      <div className="details">
-        <div className="details-image">
-          <div className="defualt">
-            <img
-              src={pokemon.sprites.front_default}
-              alt={pokemon.korean_name}
-            />
-            <img src={pokemon.sprites.back_default} alt={pokemon.korean_name} />
-          </div>
-          <div className="shiny">
-            <img src={pokemon.sprites.front_shiny} alt={pokemon.korean_name} />
-            <img src={pokemon.sprites.back_shiny} alt={pokemon.korean_name} />
-          </div>
-        </div>
-        <div className="details-about">
-          <p>
-            이름: {pokemon.korean_name}({pokemon.name})
-          </p>
-          <p>키: {pokemon.height / 10 + "m"}</p>
-          <p>무게: {pokemon.weight / 10 + "kg"}</p>
-          <p>속성: {renderTypes()}</p>
-          <p>특성: {renderAbilities()}</p>
-        </div>
-        <div className="details-stats">
-          <h3>스탯</h3>
-          <div className="stats-list-data">
-            {statList.map((stat, index) => (
-              <div key={index} className="stats-list">
-                <p>{stat.name}</p>
-                <p>{stat.value}</p>
-                <div className="stats-bar">
-                  <div
-                    className="stats-bar-fill"
-                    style={{ width: `${stat.value / 2}%` }}
-                  ></div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-      <p className="button">
-        <Link to={`/pokemon/${pokemon.id - 1}`}>
-          <span className="prev" />
-        </Link>
-        <Link to={`/pokemon/${pokemon.id + 1}`}>
-          <span className="next" />
-        </Link>
+        {isfirst ? (
+          <Link to={`/pokemon/${pokemon.id + 1}`}>
+            <span className="next" />
+          </Link>
+        ) : islast ? (
+          <Link to={`/pokemon/${pokemon.id - 1}`}>
+            <span className="prev" />
+          </Link>
+        ) : (
+          <>
+            <Link to={`/pokemon/${pokemon.id - 1}`}>
+              <span className="prev" />
+            </Link>
+            <Link to={`/pokemon/${pokemon.id + 1}`}>
+              <span className="next" />
+            </Link>
+          </>
+        )}
       </p>
     </div>
   );
